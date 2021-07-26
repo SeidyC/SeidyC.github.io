bf498451a05d2e2088e52ea4c5fe352b7467f5f3
SeidyC <82281590+SeidyC@users.noreply.github.com>
2021-07-25 21:31:39 -0400

    Create index.js

---
+++ src/containers/Home2/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Title, Subtitle, Div } from '../Home/styles'
+import { Link } from 'react-router-dom'
+
+const Home2 = () => (
+  <Div>
+      <center>
+      <img src = "http://www.freepngclipart.com/download/line/2245-line-to-use-resource-download-png.png"width="170" height="130" />
+        </center>
+    <Title>
+  Contact Information
+</Title>
+<center>
+
+<p>
+<div>
+  <img src = "https://www.logo.wine/a/logo/Gmail/Gmail-Logo.wine.svg"width="25" height="15"/>
+     :  seidy0207@gmail.com (personal)
+  </div>
+  <img src = "https://www.logo.wine/a/logo/Gmail/Gmail-Logo.wine.svg"width="25" height="15"/>
+: seidy302138@student.passaicschools.org (school)
+<div>
+  <img src = "https://www.freeiconspng.com/thumbs/snapchat-logo/get-snapchat-logo-png-pictures-6.png" width="25" height="15"/>
+  :@ seidy895
+</div>
+<img src = "http://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" width="25" height="19"/>
+: @FRoosevelt32ndP
+<div>
+  <img src = "https://cdn2.iconfinder.com/data/icons/font-awesome/1792/phone-512.png" width="15" height="15" />
+  : (***)***-****
+</div>
+<img src = "https://i.dlpng.com/static/png/5526935-download-logo-instagram-free-png-transparent-image-and-clipart-instagram-logo-clip-art-400_266_preview.png" width="26" height="15"/>
+: @seidy.hi
+</p>
+<div> ~ </div>
+    <div>
+        <Subtitle>
+<Link to="/">Home Page</Link>
+&nbsp;  &nbsp;
+<Link to="/Extracurriculars.com">Extracurriculars</Link>
+        </Subtitle>     
+    </div>
+<img src = "https://cdn140.picsart.com/277579292004211.png?r480x480" width="500" height="100"/>
+</center>
+  </Div>
+)
+export default Home2
